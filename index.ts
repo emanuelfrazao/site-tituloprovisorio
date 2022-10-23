8d68a4992d2a5faad00c29106e9b284fad596d42
emanuelfrazao <emanuel.j.frazao@gmail.com>
2022-10-23 22:53:11 +0100

    trying

--- index.ts
+++ index.ts
@@ -1,10 +1,18 @@
-import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+// import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { serve } from "https://deno.land/std/http/mod.ts";
 
-const app = new Application();
 
-app
-    .get("/hello", (_ctx) => {
-        return "Hello, Abc!";
-    })
-    .static("/", "./public")
-    .start({ port: 8000 });
+const reqHandler = async (_req: Request) => {
+    const html_contents = await Deno.readFile("./index.html");
+    return new Response(html_contents);
+};
+serve(reqHandler, { port: 8080 });
+
+// const app = new Application();
+
+// app
+//     .get("/hello", (_ctx) => {
+//         return "Hello, Abc!";
+//     })
+//     .static("/", "./public")
+//     .start({ port: 8000 });
