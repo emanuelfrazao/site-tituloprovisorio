ea5eafe7453336ddfe88acadf4a24383cceb9c00
emanuelfrazao <emanuel.j.frazao@gmail.com>
2022-10-26 05:47:17 +0100

    fixing new static files processing

--- index.ts
+++ index.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.160.0/http/server.ts";
 import staticFiles from "https://deno.land/x/static_files@1.1.6/mod.ts";
 
-const serveFiles = (req: Request) => staticFiles('public')({ 
+const serveFiles = (req: Request) => staticFiles('static')({ 
     request: req, 
     respondWith: (r: Response) => r 
 })
